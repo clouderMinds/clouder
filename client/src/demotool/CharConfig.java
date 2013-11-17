4558b8e4187c86783d619280f39e12e01247b0c4
hadoop <hadoop@ubuntu.ubuntu-domain>
2013-11-17 12:44:21 +0800

    这个类被改变了

--- client/src/demotool/CharConfig.java
+++ client/src/demotool/CharConfig.java
@@ -5,7 +5,7 @@ import java.nio.charset.Charset;
 /**
  * 定义字符集的类
  * @author hadoop
- *
+ * 我想给变一些内容，你说能不能改掉
  */
 public class CharConfig {
     public static Charset charset=Charset.forName("utf-8");
