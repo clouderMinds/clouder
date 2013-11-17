4d072c89db13f411070fbadbb0f9c0f3c4010efe
hadoop <hadoop@ubuntu.ubuntu-domain>
2013-11-17 13:11:08 +0800

    两种修改代码的方式，一个是直接修改，另一个是替换。

--- harsand/src/hxiong/dbmanager/oracle/ConnectORACLE.java
+++ harsand/src/hxiong/dbmanager/oracle/ConnectORACLE.java
@@ -1,5 +1,10 @@
 package hxiong.dbmanager.oracle;
 
+/**
+ * 这是获取oracle数据库连接对象的类
+ * @author 熊天成
+ *
+ */
 public class ConnectORACLE {
 
 }
--- harsand/src/hxiong/dbmanager/psql/ConnectPSQL.java
+++ harsand/src/hxiong/dbmanager/psql/ConnectPSQL.java
@@ -1,5 +1,12 @@
 package hxiong.dbmanager.psql;
 
+/**
+ * 这是一个获取psql数据库驱动的类，该类提供多种获取数据库驱动的方法
+ * 比如根据用户名和密码获取默认数据库的连接对象
+ * 根据用户名和密码以及数据名获取数据库连接对象
+ * @author 熊天成
+ *
+ */
 public class ConnectPSQL {
 
 }
