8f76d079f85b8cf36fea0c1729706f9057d7eb63
hadoop <hadoop@ubuntu.ubuntu-domain>
2013-11-17 15:19:35 +0800

    这是邵柯的。

---
+++ hdfsdatabase/src/managehdfs/FileManager.java
@@ -0,0 +1,15 @@
+package managehdfs;
+
+/**
+ * hdfs文件管理对象
+ * 
+ * @author 熊天成
+ *
+ */
+public class FileManager {
+    
+	private static boolean isFileExit(String f_name){
+		
+		return false;
+	}
+}
